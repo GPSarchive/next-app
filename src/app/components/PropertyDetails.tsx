94985f7195568b22582aa6bcc1cc3c19511337cf
Giorgos Spyrou <SFnader@gmail.com>
2025-03-31 12:21:56 +0300

    Initial commit: Firebase Auth + Secure Sessions+Firestore Database Sample Data

--- src/app/components/PropertyDetails.tsx
+++ src/app/components/PropertyDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './PropertyDetails.module.css';
+import styles from '@/app/components/PropertyDetails.module.css';
 
 type PropertyDetailsProps = {
   property: {
