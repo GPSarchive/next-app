fe8b6249171081f6c22adcea9a9e76459c11ef8c
Giorgos Spyrou <SFnader@gmail.com>
2025-04-14 06:23:48 +0300

    minorfix

--- src/app/components/DetailsPageComponents/Modal.tsx
+++ src/app/components/DetailsPageComponents/Modal.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
-import styles from '@/app/components/DetailsPageComponents/modal.module.css';
+import styles from '@/app/components/DetailsPageComponents/Modal.module.css';
 
 type ModalProps = {
   isOpen: boolean;
